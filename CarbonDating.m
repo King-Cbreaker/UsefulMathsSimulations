52f36676fefbd877c304bb941cbb23a4e1541eaf
King-Cbreaker <davidyjuanmunoz1234@gmail.com>
2021-01-04 14:56:44 +0100

    Delete CarbonDating.m

--- CarbonDating.m
+++
@@ -1,5 +0,0 @@
-InitialCarbonAmount = input("Initial Carbon Amount (%): ")
-FinalCarbonAmount = input("Final Carbon Amount(%): ")
-
-time = log(InitialCarbonAmount/FinalCarbonAmount) * 8267;
-printf("The sample processed died %d years ago \n", time)
